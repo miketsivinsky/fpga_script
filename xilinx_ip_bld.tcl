d446af4091011a0ce51c80f6920cb8b16edfd687
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2017-04-22 21:07:37 +0700

    change ip-config tcl structure - all job in procedures

--- xilinx_ip_bld.tcl
+++ xilinx_ip_bld.tcl
@@ -1,6 +1,30 @@
 #*******************************************************************************
 #*******************************************************************************
 
+#------------------------------------------------------------------------------
+proc ipGenPrologue { ipCoreName ipCoreOutDir DEVICE ipInfo } {
+
+	create_project -force managed_ip_project $ipCoreOutDir/managed_ip_project -part $DEVICE -ip	
+
+	set_property coreContainer.enable [dict get [$ipInfo] isPacked]  [current_project]
+	set_property target_simulator      XSim      [current_project]
+	set_property "simulator_language"  "Mixed"   [current_project]
+	set_property "target_language"     "Verilog" [current_project]
+}
+
+#------------------------------------------------------------------------------
+proc ipGenEpilogue { ipCoreName ipCoreOutDir } {
+	generate_target all [get_ips  $ipCoreName]
+	export_ip_user_files -of_objects [get_ips $ipCoreName] -sync -force -quiet
+	create_ip_run [get_ips $ipCoreName]
+	launch_runs -jobs 6 ${ipCoreName}_synth_1
+	wait_on_run ${ipCoreName}_synth_1
+	export_simulation -of_objects [get_ips $ipCoreName] -directory ${ipCoreOutDir}/ip_user_files/sim_scripts -ip_user_files_dir ${ipCoreOutDir}/ip_user_files -ipstatic_source_dir ${ipCoreOutDir}/ip_user_files/ipstatic -lib_map_path [list {modelsim=${ipCoreOutDir}/managed_ip_project/managed_ip_project.cache/compile_simlib/modelsim} {questa=${ipCoreOutDir}/managed_ip_project/managed_ip_project.cache/compile_simlib/questa}] -use_ip_compiled_libs -force -quiet
+
+	close_project
+	#touch {.ip.done}
+}
+
 #-----------------------------------
 set DEBUG_INFO 0
 
@@ -24,24 +48,15 @@ if { [file exists $ipCoreOutDir]} {
 file mkdir $ipCoreOutDir
 
 #-----------------------------------
-create_project -force managed_ip_project $ipCoreOutDir/managed_ip_project -part $DEVICE -ip
-
-set_property coreContainer.enable  1         [current_project]
-set_property target_simulator      XSim      [current_project]
-set_property "simulator_language"  "Mixed"   [current_project]
-set_property "target_language"     "Verilog" [current_project]
-
 source $IP_CFG
+if { [dict get [ipInfo] isSynth] } {
+	ipGenPrologue ${ipCoreName} ${ipCoreOutDir} ${DEVICE} "ipInfo"
+	ipCfgPrologue ${ipCoreName} ${ipCoreOutDir}
+	ipUserCfg     ${ipCoreName} ${ipCoreOutDir}
+	ipGenEpilogue ${ipCoreName} ${ipCoreOutDir}
 
-generate_target all [get_ips  $ipCoreName]
-export_ip_user_files -of_objects [get_ips $ipCoreName] -sync -force -quiet
-create_ip_run [get_ips $ipCoreName]
-launch_runs -jobs 6 ${ipCoreName}_synth_1
-wait_on_run ${ipCoreName}_synth_1
-export_simulation -of_objects [get_ips $ipCoreName] -directory ${ipCoreOutDir}/ip_user_files/sim_scripts -ip_user_files_dir ${ipCoreOutDir}/ip_user_files -ipstatic_source_dir ${ipCoreOutDir}/ip_user_files/ipstatic -lib_map_path [list {modelsim=${ipCoreOutDir}/managed_ip_project/managed_ip_project.cache/compile_simlib/modelsim} {questa=${ipCoreOutDir}/managed_ip_project/managed_ip_project.cache/compile_simlib/questa}] -use_ip_compiled_libs -force -quiet
-
-close_project
-#touch {.ip.done}
+} else {
+}
 
 #-----------------------------------
 if {$DEBUG_INFO == 1} {
