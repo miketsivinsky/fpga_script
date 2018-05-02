51696e126a80ba9f1d47a755d31e7f4b804ff020
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2018-05-02 22:10:13 +0700

    undo 'obsolete'  parameter - it is really need!

--- xilinx_prj_gen.tcl
+++ xilinx_prj_gen.tcl
@@ -78,7 +78,7 @@ set SRC_DIR           [lindex $argv 1]
 set OUT_CFG_DIR       [lindex $argv 2]
 set PRJ_NAME          [lindex $argv 3]
 set TARGET_FILE_NAME  [lindex $argv 4]
-#set DEVICE            [lindex $argv 5]
+set DEVICE            [lindex $argv 5]
 
 #-----------------------------------
 prjClean ${OUT_CFG_DIR}
@@ -89,7 +89,7 @@ source $SCRIPT_DIR/cfg_header_gen.tcl
 set CFG_DIR [pwd]
 
 #-----------------------------------
-set srcFileListStart 5
+set srcFileListStart 6
 set srcFileNum [expr $argc - $srcFileListStart]
 set srcFileList [lrange $argv $srcFileListStart end]
 
@@ -142,7 +142,7 @@ foreach ip_xci [dict get $ipLists xci] {
 
 
 #-----------------------------------
-#set_property part ${DEVICE} [current_project]
+set_property part ${DEVICE} [current_project]
 source ${CFG_DIR}/settings.tcl
 
 #-----------------------------------
@@ -179,6 +179,6 @@ if {$DEBUG_INFO == 1} {
 	puts "\[XILINX_PRJ_GEN:DEBUG\] OUT_CFG_DIR:      $OUT_CFG_DIR"
 	puts "\[XILINX_PRJ_GEN:DEBUG\] PRJ_NAME:         $PRJ_NAME"
 	puts "\[XILINX_PRJ_GEN:DEBUG\] TARGET_FILE_NAME: $TARGET_FILE_NAME"
-#	puts "\[XILINX_PRJ_GEN:DEBUG\] DEVICE:           $DEVICE"
+	puts "\[XILINX_PRJ_GEN:DEBUG\] DEVICE:           $DEVICE"
 }
 
