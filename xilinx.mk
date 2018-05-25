0f0ebe5b7c84d2ac668841376b8d4518c0a8f8a6
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2018-05-25 14:02:51 +0700

    change names for dirs 'ip', 'out'

--- xilinx.mk
+++ xilinx.mk
@@ -6,9 +6,9 @@ BIN_DIR         := $(REF_DIR)/bin
 IP_LIB_DIR      := $(REF_DIR)/ip/xilinx
 LIB_DIR         := $(REF_DIR)/lib
 
-OUT_DIR         := $(SRC_DIR)/-out
+OUT_DIR         := $(SRC_DIR)/.out
 OUT_CFG_DIR     := $(OUT_DIR)/$(CFG_NAME)
-OUT_IP_DIR      := $(OUT_CFG_DIR)/-ip
+OUT_IP_DIR      := $(OUT_CFG_DIR)/.ip
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
